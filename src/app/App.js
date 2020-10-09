1cde926bf458e96d8cfa64202b8ffaf94eba4801
MarinaAleshina <marinaproskura@gmail.com>
2020-10-09 04:42:40 +0300

    second_commit

--- src/app/App.js
+++ src/app/App.js
@@ -5,73 +5,82 @@ import TaskList from "../taskList/taskList";
 import Task from "../tasks.json";
 import Tabs from "../tab/tab";
 
+let maxId = 100;
 class App extends Component {
   state = {
     tasks: Task,
-    filter:false
+    filter: false,
   };
 
   add = (value, value1, id) => {
+    console.log(maxId);
     if ((value, value1 === "")) return;
     this.setState({
       tasks: [
         ...this.state.tasks,
-        { title: value, body: value1, complited: false, _id: id }
-      ]
+        { title: value, body: value1, complited: false, _id: maxId++ },
+      ],
     });
   };
 
-  changeStatus = (id,name) => {
+  changeStatus = (id, name) => {
     const { tasks = [] } = this.state;
     // eslint-disable-next-line array-callback-return
-    tasks.some((el )=> {
-      if (el._id === id) 
-        el[name] = !el[name] 
+    tasks.some((el) => {
+      if (el._id === id) el[name] = !el[name];
     });
 
     this.setState({
-      tasks
+      tasks,
     });
   };
 
-  remove=(id)=>{
-const {tasks}=this.state
-const index=tasks.findIndex(el=>el._id===id)
-const newArr=[
-  ...tasks.slice(0,index),
-...tasks.slice(index+1,tasks.length)
-]
-this.setState({tasks:newArr})
-  }
+  remove = (id) => {
+    const { tasks } = this.state;
+    const index = tasks.findIndex((el) => el._id === id);
+    const newArr = [
+      ...tasks.slice(0, index),
+      ...tasks.slice(index + 1, tasks.length),
+    ];
+    this.setState({ tasks: newArr });
+  };
 
-  filterFunc=(type)=>{
-  let filter=false;
-  if(filter!=="all") filter=type;
-  this.setState({filter})
-  }
+  filterFunc = (type) => {
+    let filter = false;
+    if (filter !== "all") filter = type;
+    this.setState({ filter });
+  };
 
-  useTabs=(tasks,filter)=>{
-    if(!filter || filter==="all") return tasks;
-    const newlist=tasks.filter(el=>
-    {return el[filter]})
+  useTabs = (tasks, filter) => {
+    if (!filter || filter === "all") return tasks;
+    const newlist = tasks.filter((el) => {
+      return el[filter];
+    });
 
-    return newlist
-  }
+    return newlist;
+  };
 
   render() {
-    const { tasks,filter } = this.state;
+    const { tasks, filter } = this.state;
     const newCount = tasks.length;
-    const tab=this.useTabs(tasks,filter)
-   
+    const tab = this.useTabs(tasks, filter);
+
     return (
       <div className={"wrap"}>
-        <Header count={newCount} />
-        <Main add={this.add} />
-        <div className={"container-tasks"}>
-          <Tabs filterFunc={this.filterFunc} filter={filter}/>
-          <ul className={"task-container"}>
-            <TaskList tasks={tab} changeStatus={this.changeStatus} remove={this.remove}/>
-          </ul>
+        <div className="modal">
+          <Header count={newCount} />
+          <Main add={this.add} />
+          <div className={"container-tasks"}>
+            <Tabs filterFunc={this.filterFunc} filter={filter} />
+            <ul className={"task-container"}>
+              <TaskList
+                tasks={tab}
+                changeStatus={this.changeStatus}
+                remove={this.remove}
+              />
+            </ul>
+          </div>
+          <button className={"menu-button"}>show menu</button>
         </div>
       </div>
     );
